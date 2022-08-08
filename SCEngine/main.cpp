90e1068dc8287d42cea9a557589f64c4e56e07ca
SSSxCCC <570909477@qq.com>
2022-08-08 19:08:17 +0800

    Add model, view, projection matrix.

--- SCEngine/main.cpp
+++ SCEngine/main.cpp
@@ -4,6 +4,10 @@
 #include "imgui_backends/imgui_impl_glfw.h"
 #include "imgui_backends/imgui_impl_opengl3.h"
 #include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include <string>
 #include <fstream>
@@ -73,17 +77,17 @@ public:
 		glUseProgram(ID);
 	}
 	// utility uniform functions
-	// ------------------------------------------------------------------------
 	void setBool(const std::string& name, bool value) const {
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 	}
-	// ------------------------------------------------------------------------
 	void setInt(const std::string& name, int value) const {
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 	}
-	// ------------------------------------------------------------------------
 	void setFloat(const std::string& name, float value) const {
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	}
+	void setMat4(const std::string& name, float* value) const {
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, value);
 	}
 
 private:
@@ -112,19 +116,127 @@ private:
 
 class Camera {
 public:
-	Camera() {
-		posX = 0;
-		posY = 0;
-	}
+	Camera(int width, int height) : mWidth(width), mHeight(height) { }
+	void getSize(int& outWidth, int& outHeight) { outWidth = mWidth; outHeight = mHeight; }
+	void setSize(int width, int height) { mWidth = width; mHeight = height; }
 private:
-	int posX, posY;
+	int mWidth, mHeight;
+	int mCenterX = 0, mCenterY = 0;
+	float mZoom = 1.0f;
 };
-
+Camera gCamera(800, 600);
 
 void glfwErrorCallback(int error, const char* description) {
 	fprintf(stderr, "GLFW error occured. Code: %d. Description: %s\n", error, description);
 }
 
+static void ScrollCallback(GLFWwindow* window, double dx, double dy) {
+	ImGui_ImplGlfw_ScrollCallback(window, dx, dy);
+	if (ImGui::GetIO().WantCaptureMouse) {
+		return;
+	}
+
+	//if (dy > 0) {
+	//	g_camera.m_zoom /= 1.1f;
+	//}
+	//else {
+	//	g_camera.m_zoom *= 1.1f;
+	//}
+}
+
+static void WindowSizeCallback(GLFWwindow*, int width, int height) {
+	gCamera.setSize(width, height);
+}
+
+static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
+	if (ImGui::GetIO().WantCaptureKeyboard) {
+		return;
+	}
+
+	if (action == GLFW_PRESS) {
+		switch (key) {
+		case GLFW_KEY_ESCAPE:
+			std::cout << "GLFW_KEY_ESCAPE" << std::endl;
+			// Quit
+			glfwSetWindowShouldClose(window, GL_TRUE);
+			break;
+		case GLFW_KEY_LEFT:
+			std::cout << "GLFW_KEY_LEFT" << std::endl;
+			break;
+		case GLFW_KEY_RIGHT:
+			std::cout << "GLFW_KEY_RIGHT" << std::endl;
+			break;
+		case GLFW_KEY_DOWN:
+			std::cout << "GLFW_KEY_DOWN" << std::endl;
+			break;
+		case GLFW_KEY_UP:
+			std::cout << "GLFW_KEY_UP" << std::endl;
+			break;
+		case GLFW_KEY_HOME:
+			std::cout << "GLFW_KEY_HOME" << std::endl;
+			break;
+		default:
+			break;
+		}
+	} else if (action == GLFW_RELEASE) {
+		std::cout << "GLFW_RELEASE" << std::endl;
+	}
+	// else GLFW_REPEAT
+}
+
+static void CharCallback(GLFWwindow* window, unsigned int c) {
+	ImGui_ImplGlfw_CharCallback(window, c);
+}
+
+static void MouseButtonCallback(GLFWwindow* window, int32 button, int32 action, int32 mods) {
+	ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
+
+	/*double xd, yd;
+	glfwGetCursorPos(window, &xd, &yd);
+	b2Vec2 ps((float)xd, (float)yd);
+
+	// Use the mouse to move things around.
+	if (button == GLFW_MOUSE_BUTTON_1) {
+		//<##>
+		//ps.Set(0, 0);
+		b2Vec2 pw = g_camera.ConvertScreenToWorld(ps);
+		if (action == GLFW_PRESS) {
+			if (mods == GLFW_MOD_SHIFT) {
+				s_test->ShiftMouseDown(pw);
+			} else {
+				s_test->MouseDown(pw);
+			}
+		}
+
+		if (action == GLFW_RELEASE) {
+			s_test->MouseUp(pw);
+		}
+	} else if (button == GLFW_MOUSE_BUTTON_2) {
+		if (action == GLFW_PRESS) {
+			s_clickPointWS = g_camera.ConvertScreenToWorld(ps);
+			s_rightMouseDown = true;
+		}
+
+		if (action == GLFW_RELEASE) {
+			s_rightMouseDown = false;
+		}
+	}*/
+}
+
+static void CursorPosCallback(GLFWwindow*, double xd, double yd) {
+	/*b2Vec2 ps((float)xd, (float)yd);
+
+	b2Vec2 pw = g_camera.ConvertScreenToWorld(ps);
+	s_test->MouseMove(pw);
+
+	if (s_rightMouseDown) {
+		b2Vec2 diff = pw - s_clickPointWS;
+		g_camera.m_center.x -= diff.x;
+		g_camera.m_center.y -= diff.y;
+		s_clickPointWS = g_camera.ConvertScreenToWorld(ps);
+	}*/
+}
 
 int main() {
 	b2Vec2 gravity(0.0f, -10.0f);
@@ -175,7 +287,8 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	int width = 800, height = 600;
+	int width, height;
+	gCamera.getSize(width, height);
 	GLFWwindow* mainWindow = glfwCreateWindow(width, height, "SCEngine", NULL, NULL);
 
 	if (mainWindow == NULL) {
@@ -190,12 +303,12 @@ int main() {
 	printf("GLAD %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
 	printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
 
-	/*glfwSetScrollCallback(mainWindow, ScrollCallback);
-	glfwSetWindowSizeCallback(mainWindow, ResizeWindowCallback);
+	glfwSetScrollCallback(mainWindow, ScrollCallback);
+	glfwSetWindowSizeCallback(mainWindow, WindowSizeCallback);
 	glfwSetKeyCallback(mainWindow, KeyCallback);
 	glfwSetCharCallback(mainWindow, CharCallback);
 	glfwSetMouseButtonCallback(mainWindow, MouseButtonCallback);
-	glfwSetCursorPosCallback(mainWindow, MouseMotionCallback);*/
+	glfwSetCursorPosCallback(mainWindow, CursorPosCallback);
 
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -240,7 +353,17 @@ int main() {
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+		glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+		glm::mat4 model = glm::rotate(glm::scale(glm::mat4(1.0f), glm::vec3(400.0f, 300.0f, 1.0f)), glm::radians(15.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		glm::mat4 projection = glm::ortho((float)(-width/2), (float) (width/2), (float)(-height/2), (float) (height/2), -1000.0f, 1000.0f);
+		std::cout << glm::to_string(projection) << std::endl;
 		shader.use();
+		shader.setMat4("model", glm::value_ptr(model));
+		shader.setMat4("view", glm::value_ptr(view));
+		shader.setMat4("projection", glm::value_ptr(projection));
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
--- SCEngine/shader.vs
+++ SCEngine/shader.vs
@@ -5,12 +5,12 @@ layout (location = 1) in vec3 aColor;
 
 out vec3 color;
 
-//uniform mat4 model;
-//uniform mat4 view;
-//uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
-   //gl_Position = projection * view * model * vec4(aPos, 1.0);
-   gl_Position = vec4(aPos, 1.0);
+   gl_Position = projection * view * model * vec4(aPos, 1.0);
+   //gl_Position = vec4(aPos, 1.0);
    color = aColor;
 }
