5e82f816b16588b5c6447e3c9dbd8c5ee5b18f25
SSSxCCC <570909477@qq.com>
2022-08-25 16:30:07 +0800

    Add explain to STATIC_BLOCK

--- SCEngine/utility/StaticBlock.h
+++ SCEngine/utility/StaticBlock.h
@@ -4,6 +4,17 @@
 #define M_CON(A, B) M_CON_(A, B)
 #define M_CON_(A, B) A##B
 
+/*
+A mechanism for executing code statically.
+
+Usage example:
+STATIC_BLOCK {
+	// static code start
+	std::cout << "static_block!" << std::endl;
+	// static code end
+	return 0;
+}();
+*/
 #define STATIC_BLOCK \
         [[maybe_unused]] static const auto M_CON(_static_block,__LINE__) = []()
 
